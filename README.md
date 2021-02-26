7fcf776b07076a8848520d0e891cf0c7eb8dbcc1
somleff <dmytro.somliev@ibm.com>
2021-02-26 12:47:36 +0100

    Deploy Changes

--- README.md
+++ README.md
@@ -8,6 +8,18 @@ All idea, tasks and some solutions were taken from ["The Modern JavaScript Bootc
  - Languages: JavaScript
  - NPM packages: Live Server, Moment
 
+
+# Apps Examples
+Check the applications, that were made during the course at the following links:
+- [Note App](https://somleff-notes.netlify.app)
+- [Hangman](https://somleff-hangman.netlify.app)
+- [TodosApp](https://stupid-cry.surge.sh)
+
+P.S. NoteApp and Hangman hosted on https://app.netlify.com/. TodosApp deployed by using https://surge.sh.
+ <br />
+ > For educational purposes only
+
+
 # Course Description
 Here is the list of main topics covered by the course.
  - JavaScript Basics: Variables and Flow Control
@@ -18,5 +30,5 @@ Here is the list of main topics covered by the course.
  - Data Storage, Libraries
  - Arrow Functions
  - Asynchronous JavaScript
- - CSS themes for Apps
+ - CSS themes for Apps and Hosting Applications
  - Babel, Webpack
--- hangman/scripts/requests.js
+++ hangman/scripts/requests.js
@@ -1,6 +1,6 @@
 // Async/Await Example
 const getPuzzle = async (wordCount) => {
-    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`)
 
     if (response.status === 200) {
         const data = await response.json()
@@ -12,7 +12,7 @@ const getPuzzle = async (wordCount) => {
 }
 
 const getCountry = async (countryCode) => {
-    const response = await fetch('http://restcountries.eu/rest/v2/all')
+    const response = await fetch('//restcountries.eu/rest/v2/all')
 
     if (response.status === 200) {
         const data = await response.json()
@@ -25,7 +25,7 @@ const getCountry = async (countryCode) => {
 }
 
 const getLocation = async () => {
-    const response = await fetch('http://ipinfo.io/json?token=cba8fbdff5bd16')
+    const response = await fetch('//ipinfo.io/json?token=cba8fbdff5bd16')
 
         if (response.status === 200) {
             return response.json()
