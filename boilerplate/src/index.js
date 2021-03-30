dde8c8940350a7894de8acf2707eb14da2db97c6
somleff <somleff@gmail.com>
2021-03-30 11:55:38 +0200

    The Rest Parameter

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -8,3 +8,24 @@ console.log(add(32, 3));
 console.log(scream(`i love ${name}`));
 
 console.log(otherSquare(3));
+
+// Rest Parameter
+const calculateAverage = (thing, ...numbers) => {
+    // return (number1 + number2) / 2;
+    let sum = 0;
+    numbers.forEach((num) => sum += num);
+    // return sum / numbers.length;
+    const average = sum / numbers.length;
+    return `The average ${thing} is ${average}`;
+};
+console.log(calculateAverage('age', 0, 100, 88, 64));
+
+// Task
+const printTeam = (teamName, coach, ...players) => {
+    console.log(`Team: ${teamName}`);
+    console.log(`Coach: ${coach}`);
+    // console.log(`Team: ${players}`);
+    console.log(`Team: ${players.join(', ')}`);
+};
+// printTeam('Liberty', 'JuJu', ['Juka','Buka', 'Duka']);
+printTeam('Liberty', 'JuJu', 'Juka','Buka', 'Duka');
